8a8fe42c4dde8781cfb9e3100555595684dc3ef5
oualaeddine <berrehaloualaeddine@gmail.com>
2016-11-16 22:22:46 +0100

    third commit

--- app/src/main/java/ntic/berrehal/oualaeddine/robot/MainMap.java
+++ app/src/main/java/ntic/berrehal/oualaeddine/robot/MainMap.java
@@ -1,7 +1,11 @@
 package ntic.berrehal.oualaeddine.robot;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 
 public class MainMap extends AppCompatActivity {
 
@@ -9,5 +13,31 @@ public class MainMap extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_map);
+    }
+
+    public class RobotAdapter extends BaseAdapter{
+
+        private Context context;
+        private Integer robot = R.drawable.
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            return null;
+        }
     }
 }
